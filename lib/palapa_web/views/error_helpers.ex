ae065399866e3f925e09d6368e80284ab0b7a8fc
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-01-04 16:41:56 +0100

    Extract form components

--- lib/palapa_web/views/error_helpers.ex
+++ lib/palapa_web/views/error_helpers.ex
@@ -8,7 +8,7 @@ defmodule PalapaWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field, class) do
+  def error_tag(form, field, class \\ "error") do
     Enum.map(Keyword.get_values(form.errors, field), fn (error) ->
       content_tag :span, translate_error(error), class: class
     end)
