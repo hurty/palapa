6c534853dbbfefc1990125d3baf66d80e9e39fb0
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-02-09 12:48:54 +0100

    Use separate db creds instead of uri

--- config/prod.exs
+++ config/prod.exs
@@ -17,14 +17,17 @@ config :palapa, PalapaWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "palapa.cleverapps.io", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
 config :palapa, Palapa.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("POSTGRESQL_ADDON_URI"),
+  hostname: System.get_env("POSTGRESQL_ADDON_HOST"),
+  username: System.get_env("POSTGRESQL_ADDON_USER"),
+  database: System.get_env("POSTGRESQL_ADDON_DB"),
+  password: System.get_env("POSTGRESQL_ADDON_PASSWORD"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
