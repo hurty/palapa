c8936d4b1442e46cfa7807374318c17a1e9467a6
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-07-06 15:44:01 +0200

    opening popover to bottom-end fits for mobile and desktop

--- assets/js/controllers/popover_controller.js
+++ assets/js/controllers/popover_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
 
   connect() {
     this.popover = new PopperJs(this.buttonTarget, this.contentTarget, {
-      placement: "auto-start",
+      placement: "bottom-end",
       modifiers: {
         arrow: {
           element: this.arrowTarget
