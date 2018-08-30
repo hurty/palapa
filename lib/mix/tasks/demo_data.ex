fc08c94f495fa0a8f1e79bf6aad61466f002e416
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-08-30 16:09:37 +0200

    Optionnaly drop database before inserting demo data

--- lib/mix/tasks/demo_data.ex
+++ lib/mix/tasks/demo_data.ex
@@ -2,10 +2,17 @@ defmodule Mix.Tasks.Demo do
   use Mix.Task
 
   @shortdoc "Insert a demo workspace"
-  def run(_) do
-    Mix.Task.run("ecto.drop")
-    Mix.Task.run("ecto.create")
-    Mix.Task.run("ecto.migrate")
+  def run(args) do
+    parsed = OptionParser.parse(args, switches: [drop: :boolean])
+    IO.inspect(parsed)
+
+    case parsed do
+      {[drop: true], _, _} ->
+        Mix.Task.run("ecto.drop")
+        Mix.Task.run("ecto.create")
+        Mix.Task.run("ecto.migrate")
+    end
+
     Mix.Task.run("app.start")
     Palapa.Factory.insert_pied_piper!(:full)
   end
