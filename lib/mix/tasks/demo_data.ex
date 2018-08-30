9db15f110b153696c481c89f0f628ee5e41d5ce2
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-08-30 16:17:48 +0200

    Fix demo data when no arg is given

--- lib/mix/tasks/demo_data.ex
+++ lib/mix/tasks/demo_data.ex
@@ -4,13 +4,15 @@ defmodule Mix.Tasks.Demo do
   @shortdoc "Insert a demo workspace"
   def run(args) do
     parsed = OptionParser.parse(args, switches: [drop: :boolean])
-    IO.inspect(parsed)
 
     case parsed do
       {[drop: true], _, _} ->
         Mix.Task.run("ecto.drop")
         Mix.Task.run("ecto.create")
         Mix.Task.run("ecto.migrate")
+
+      _ ->
+        nil
     end
 
     Mix.Task.run("app.start")
