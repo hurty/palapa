3355ca1129d637f89055381398bb031665242ae5
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-09-03 11:03:10 +0200

    Enable https on clever

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :palapa, PalapaWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "palapa.cleverapps.io"],
+  url: [scheme: "https", host: "palapa.cleverapps.io", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
