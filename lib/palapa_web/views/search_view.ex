9f758cd208f8eeaee168bd56d930f5963ea15542
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2018-12-21 19:23:41 +0100

    Implements search results specifics in each appropriate template

--- lib/palapa_web/views/search_view.ex
+++ lib/palapa_web/views/search_view.ex
@@ -1,24 +1,6 @@
 defmodule PalapaWeb.SearchView do
   use PalapaWeb, :view
 
-  def resource_path(conn, search_result) do
-    organization = conn.assigns.current_organization
-
-    case search_result.resource_type do
-      "team" ->
-        member_path(conn, :index, organization, team_id: search_result.resource_id)
-
-      "member" ->
-        member_path(conn, :show, organization, search_result.resource_id)
-
-      "message" ->
-        message_path(conn, :show, organization, search_result.resource_id)
-
-      "page" ->
-        document_page_path(conn, :show, organization, search_result.resource_id)
-    end
-  end
-
   def search_result_template_path(search_result) do
     case search_result.resource_type do
       "team" ->
@@ -32,22 +14,6 @@ defmodule PalapaWeb.SearchView do
 
       "page" ->
         "search_result_page.html"
-    end
-  end
-
-  def formatted_resource_type(search_result) do
-    case search_result.resource_type do
-      "team" ->
-        "Team"
-
-      "member" ->
-        "Member"
-
-      "message" ->
-        "Message"
-
-      "page" ->
-        "Document page"
     end
   end
 end
