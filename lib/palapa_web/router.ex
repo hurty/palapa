13066666d3e4ff99ba3a6f20e8efe44345fff04f
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-02-15 17:26:01 +0100

    Organization root path is the dashboard page

--- lib/palapa_web/router.ex
+++ lib/palapa_web/router.ex
@@ -45,7 +45,7 @@ defmodule PalapaWeb.Router do
       get("/sessions/switch_organization", SessionController, :switch_organization)
       get("/sessions/switcher", SessionController, :switcher)
 
-      get("/dashboard", DashboardController, :index)
+      get("/", DashboardController, :index)
       get("/search", SearchController, :index)
 
       resources("/attachments", AttachmentController, only: [:create, :delete]) do
