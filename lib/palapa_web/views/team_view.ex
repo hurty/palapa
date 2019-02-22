5600901c2696d33b3482e646fdb3aded6214dc33
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-02-22 16:02:30 +0100

    Responsive version for documents index

--- lib/palapa_web/views/team_view.ex
+++ lib/palapa_web/views/team_view.ex
@@ -9,7 +9,7 @@ defmodule PalapaWeb.TeamView do
       team.name,
       href:
         Router.Helpers.member_path(conn, :index, conn.assigns.current_organization, team_id: team),
-      class: "tag"
+      class: "tag tag-team"
     )
   end
 
