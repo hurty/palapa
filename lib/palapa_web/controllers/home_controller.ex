ecfc820ecb4648f67deadeddc9e65cb29d0c98bf
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-03-08 16:06:04 +0100

    Tidy switcher

--- lib/palapa_web/controllers/home_controller.ex
+++ lib/palapa_web/controllers/home_controller.ex
@@ -1,11 +1,14 @@
 defmodule PalapaWeb.HomeController do
   use PalapaWeb, :controller
 
+  alias Palapa.Accounts
+
   plug(:put_layout, "public.html")
 
   def index(conn, _params) do
-    if current_member() do
-      redirect(conn, to: dashboard_path(conn, :index, current_organization()))
+    if current_account() do
+      organization = Accounts.main_organization(current_account())
+      redirect(conn, to: dashboard_path(conn, :index, organization))
     else
       render(conn, "index.html")
     end
