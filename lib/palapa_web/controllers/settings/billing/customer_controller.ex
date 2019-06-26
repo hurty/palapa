22c6bac8dd8f2ee673b6f44a1880ed4a619271be
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-06-26 12:08:50 +0200

    Restrict billing settings to owners

--- lib/palapa_web/controllers/settings/billing/customer_controller.ex
+++ lib/palapa_web/controllers/settings/billing/customer_controller.ex
@@ -5,7 +5,7 @@ defmodule PalapaWeb.Settings.Billing.CustomerController do
   alias Palapa.Billing.Customer
 
   plug :put_navigation, "settings"
-  plug(:put_common_breadcrumbs)
+  plug :put_common_breadcrumbs
 
   def put_common_breadcrumbs(conn, _params) do
     conn
@@ -21,86 +21,94 @@ defmodule PalapaWeb.Settings.Billing.CustomerController do
   end
 
   def new(conn, _) do
-    customer_changeset = Billing.change_customer_infos(%Customer{})
+    with :ok <- permit(Billing, :update_billing, current_member()) do
+      customer_changeset = Billing.change_customer_infos(%Customer{})
 
-    conn
-    |> put_breadcrumb("Upgrade your account", customer_path(conn, :new, current_organization()))
-    |> render("new.html", customer_changeset: customer_changeset)
+      conn
+      |> put_breadcrumb("Upgrade your account", customer_path(conn, :new, current_organization()))
+      |> render("new.html", customer_changeset: customer_changeset)
+    end
   end
 
   def create(conn, %{"customer" => customer_attrs}) do
-    case Billing.create_customer_and_synchronize_subscription(
-           current_organization(),
-           customer_attrs
-         ) do
-      {:ok, result} ->
-        case Billing.payment_next_action(result.stripe_subscription) do
-          :requires_action ->
-            client_secret =
-              result.stripe_subscription["latest_invoice"]["payment_intent"]["client_secret"]
+    with :ok <- permit(Billing, :update_billing, current_member()) do
+      case Billing.create_customer_and_synchronize_subscription(
+             current_organization(),
+             customer_attrs
+           ) do
+        {:ok, result} ->
+          case Billing.payment_next_action(result.stripe_subscription) do
+            :requires_action ->
+              client_secret =
+                result.stripe_subscription["latest_invoice"]["payment_intent"]["client_secret"]
 
-            redirect(conn,
-              to:
-                payment_authentication_path(conn, :new, current_organization(),
-                  client_secret: client_secret
-                )
-            )
+              redirect(conn,
+                to:
+                  payment_authentication_path(conn, :new, current_organization(),
+                    client_secret: client_secret
+                  )
+              )
 
-          :requires_payment_method ->
-            nil
+            :requires_payment_method ->
+              nil
 
-          # Ask just for the card details
+            # Ask just for the card details
 
-          :ok ->
-            redirect(conn, to: customer_path(conn, :show, current_organization()))
-        end
+            :ok ->
+              redirect(conn, to: customer_path(conn, :show, current_organization()))
+          end
 
-      {:error, :customer, customer_changeset, _changes_so_far} ->
-        render(conn, "new.html", customer_changeset: customer_changeset)
+        {:error, :customer, customer_changeset, _changes_so_far} ->
+          render(conn, "new.html", customer_changeset: customer_changeset)
 
-      {:error, :stripe_customer, error, _changes} ->
-        conn
-        |> put_flash(:error, error.message)
-        |> redirect(to: customer_path(conn, :new, current_organization()))
+        {:error, :stripe_customer, error, _changes} ->
+          conn
+          |> put_flash(:error, error.message)
+          |> redirect(to: customer_path(conn, :new, current_organization()))
+      end
     end
   end
 
   def edit(conn, _) do
-    conn =
-      conn
-      |> put_breadcrumb(
-        "Update billing information",
-        customer_path(conn, :edit, current_organization())
-      )
+    with :ok <- permit(Billing, :update_billing, current_member()) do
+      conn =
+        conn
+        |> put_breadcrumb(
+          "Update billing information",
+          customer_path(conn, :edit, current_organization())
+        )
 
-    customer = Billing.get_customer(current_organization())
+      customer = Billing.get_customer(current_organization())
 
-    if customer do
-      customer_changeset = Billing.change_customer_infos(customer)
-      render(conn, "edit.html", customer_changeset: customer_changeset)
-    else
-      conn
-      |> put_flash(
-        :error,
-        "This workspace does not have a paid subscription / billing information"
-      )
-      |> redirect(to: customer_path(conn, :show, current_organization()))
+      if customer do
+        customer_changeset = Billing.change_customer_infos(customer)
+        render(conn, "edit.html", customer_changeset: customer_changeset)
+      else
+        conn
+        |> put_flash(
+          :error,
+          "This workspace does not have a paid subscription / billing information"
+        )
+        |> redirect(to: customer_path(conn, :show, current_organization()))
+      end
     end
   end
 
   def update(conn, %{"customer" => customer_attrs}) do
-    customer = Billing.get_customer(current_organization())
+    with :ok <- permit(Billing, :update_billing, current_member()) do
+      customer = Billing.get_customer(current_organization())
 
-    case Billing.update_customer_infos(customer, customer_attrs) do
-      {:ok, _customer} ->
-        conn
-        |> put_flash(:success, "Billing information has been updated successfully")
-        |> redirect(to: customer_path(conn, :show, current_organization()))
+      case Billing.update_customer_infos(customer, customer_attrs) do
+        {:ok, _customer} ->
+          conn
+          |> put_flash(:success, "Billing information has been updated successfully")
+          |> redirect(to: customer_path(conn, :show, current_organization()))
 
-      {:error, customer_changeset} ->
-        conn
-        |> put_flash(:error, "Billing information couldn't be updated")
-        |> render("edit.html", customer_changeset: customer_changeset)
+        {:error, customer_changeset} ->
+          conn
+          |> put_flash(:error, "Billing information couldn't be updated")
+          |> render("edit.html", customer_changeset: customer_changeset)
+      end
     end
   end
 end
---
+++ test/palapa_web/controllers/settings/billing/customer_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule PalapaWeb.Settings.Billing.CustomerControllerTest do
+  use PalapaWeb.ConnCase
+
+  alias Palapa.Repo
+  alias Palapa.Billing
+
+  describe "as owner" do
+    setup do
+      workspace = insert_pied_piper!()
+      conn = login(workspace.richard)
+      {:ok, conn: conn, workspace: workspace}
+    end
+
+    test "an owner can access the billing settings", %{conn: conn, workspace: workspace} do
+      conn = get(conn, customer_path(conn, :show, workspace.organization))
+      assert html_response(conn, 200)
+    end
+  end
+
+  describe "as member" do
+    setup do
+      workspace = insert_pied_piper!()
+      conn = login(workspace.gilfoyle)
+      {:ok, conn: conn, workspace: workspace}
+    end
+
+    test "a member cannot access the billing settings", %{conn: conn, workspace: workspace} do
+      conn = get(conn, customer_path(conn, :show, workspace.organization))
+      assert html_response(conn, 403)
+    end
+  end
+end
