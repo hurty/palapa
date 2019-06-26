d0b3f4dcb3b954a10430ae29a8fbfba85fe9c24f
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-06-26 17:41:07 +0200

    Properly display navigation sidebars

--- lib/palapa_web/controllers/settings/billing/customer_controller.ex
+++ lib/palapa_web/controllers/settings/billing/customer_controller.ex
@@ -5,7 +5,12 @@ defmodule PalapaWeb.Settings.Billing.CustomerController do
   alias Palapa.Billing.Customer
 
   plug :put_navigation, "settings"
+  plug :put_settings_navigation, "billing"
   plug :put_common_breadcrumbs
+
+  def put_settings_navigation(conn, params) do
+    assign(conn, :settings_navigation, params)
+  end
 
   def put_common_breadcrumbs(conn, _params) do
     conn
--- lib/palapa_web/controllers/settings/workspace/workspace_controller.ex
+++ lib/palapa_web/controllers/settings/workspace/workspace_controller.ex
@@ -2,7 +2,12 @@ defmodule PalapaWeb.Settings.WorkspaceController do
   use PalapaWeb, :controller
 
   plug :put_navigation, "settings"
-  plug(:put_common_breadcrumbs)
+  plug :put_settings_navigation, "workspace"
+  plug :put_common_breadcrumbs
+
+  def put_settings_navigation(conn, value) do
+    assign(conn, :settings_navigation, value)
+  end
 
   def put_common_breadcrumbs(conn, _params) do
     conn
