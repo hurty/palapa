28f39b181446a74229d33769c31a70a5eab7365c
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-08-28 11:28:29 +0200

    Add more password reset tests

--- test/palapa_web/controllers/password_reset_controller_test.exs
+++ test/palapa_web/controllers/password_reset_controller_test.exs
@@ -2,8 +2,6 @@ defmodule PalapaWeb.PasswordResetControllerTest do
   use PalapaWeb.ConnCase
   use Bamboo.Test, shared: true
 
-  alias Palapa.Repo
-
   setup do
     workspace = insert_pied_piper!()
     conn = build_conn()
@@ -15,14 +13,14 @@ defmodule PalapaWeb.PasswordResetControllerTest do
     assert html_response(conn, 200) =~ "Password reset"
   end
 
-  test "ask for a reset link with an existing email", %{conn: conn, workspace: workspace} do
+  test "ask for a reset link with an existing email", %{conn: conn} do
     payload = %{"password_reset" => %{"email" => "richard.hendricks@piedpiper.com"}}
     conn = post(conn, password_reset_path(conn, :create, payload))
     assert html_response(conn, 200) =~ "An email has been sent"
     assert_email_delivered_with(to: [nil: "richard.hendricks@piedpiper.com"])
   end
 
-  test "ask for a reset link with non-existing email", %{conn: conn, workspace: workspace} do
+  test "ask for a reset link with non-existing email", %{conn: conn} do
     payload = %{"password_reset" => %{"email" => "bad@mail.org"}}
     conn = post(conn, password_reset_path(conn, :create, payload))
 
@@ -31,10 +29,99 @@ defmodule PalapaWeb.PasswordResetControllerTest do
     assert_no_emails_delivered()
   end
 
-  test "ask for a reset link with invalid email address", %{conn: conn, workspace: workspace} do
+  test "ask for a reset link with invalid email address", %{conn: conn} do
     payload = %{"password_reset" => %{"email" => "invalidmail.org"}}
     conn = post(conn, password_reset_path(conn, :create, payload))
     assert html_response(conn, 200) =~ "Please enter a valid email address"
     assert_no_emails_delivered()
+  end
+
+  test "use link with secret token to display reset password form", %{
+    conn: conn,
+    workspace: workspace
+  } do
+    {:ok, token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+    conn = get(conn, password_reset_path(conn, :edit, %{"password_reset_token" => token}))
+    assert html_response(conn, 200)
+  end
+
+  test "reset password successfully", %{conn: conn, workspace: workspace} do
+    {:ok, token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+
+    payload = %{
+      "password" => %{
+        "password" => "p4ssw0rd",
+        "password_confirmation" => "p4ssw0rd",
+        "password_reset_token" => token
+      }
+    }
+
+    conn = patch(conn, password_reset_path(conn, :update, payload))
+    assert redirected_to(conn, 302)
+  end
+
+  test "reset password too short", %{conn: conn, workspace: workspace} do
+    {:ok, token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+
+    payload = %{
+      "password" => %{
+        "password" => "p4ss",
+        "password_confirmation" => "p4ss",
+        "password_reset_token" => token
+      }
+    }
+
+    conn = patch(conn, password_reset_path(conn, :update, payload))
+    assert html_response(conn, 200) =~ "should be at least 8 character(s)"
+  end
+
+  test "reset password wrong confirmation", %{conn: conn, workspace: workspace} do
+    {:ok, token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+
+    payload = %{
+      "password" => %{
+        "password" => "p4ssw0rd",
+        "password_confirmation" => "p4sswoooord",
+        "password_reset_token" => token
+      }
+    }
+
+    conn = patch(conn, password_reset_path(conn, :update, payload))
+    assert html_response(conn, 200) =~ "Password confirmation does not match"
+  end
+
+  test "reset password with invalid token", %{conn: conn, workspace: workspace} do
+    {:ok, _token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+
+    payload = %{
+      "password" => %{
+        "password" => "p4ssw0rd",
+        "password_confirmation" => "p4ssw0rd",
+        "password_reset_token" => "INVALID_TOKEN"
+      }
+    }
+
+    conn = patch(conn, password_reset_path(conn, :update, payload))
+    assert html_response(conn, 200) =~ "This reset token is invalid"
+  end
+
+  test "reset password with expired token", %{conn: conn, workspace: workspace} do
+    {:ok, token} = Palapa.Accounts.generate_password_reset_token(workspace.richard.account)
+    yesterday = Timex.now() |> Timex.shift(days: -1) |> DateTime.truncate(:second)
+
+    workspace.richard.account
+    |> Ecto.Changeset.change(%{password_reset_at: yesterday})
+    |> Palapa.Repo.update!()
+
+    payload = %{
+      "password" => %{
+        "password" => "p4ssw0rd",
+        "password_confirmation" => "p4ssw0rd",
+        "password_reset_token" => token
+      }
+    }
+
+    conn = patch(conn, password_reset_path(conn, :update, payload))
+    assert html_response(conn, 200) =~ "This reset token is invalid"
   end
 end
