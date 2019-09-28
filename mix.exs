075bcb00bcf390394f2905cf1e60561cf5720712
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-09-28 11:11:18 +0200

    conf on par with last phoenix default

--- mix.exs
+++ mix.exs
@@ -35,9 +35,8 @@ defmodule Palapa.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:prod), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
