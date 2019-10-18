761b2ee14b5fc08766ee36dab90ab3dd85ba74cc
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-10-18 19:01:02 +0200

    Hide deleted workspaces

--- test/palapa_web/controllers/authentication_test.exs
+++ test/palapa_web/controllers/authentication_test.exs
@@ -9,98 +9,116 @@ defmodule PalapaWeb.AuthenticationTest do
     organization_name: "Pied Piper"
   }
 
-  setup %{conn: conn} do
-    conn =
-      conn
-      |> bypass_through(PalapaWeb.Router, :browser)
-      |> get("/")
+  describe "account authentication" do
+    setup %{conn: conn} do
+      conn =
+        conn
+        |> bypass_through(PalapaWeb.Router, :browser)
+        |> get("/")
 
-    {:ok, %{conn: conn}}
+      {:ok, %{conn: conn}}
+    end
+
+    test "enforce_authentication halts when no current_account exists", %{conn: conn} do
+      conn = Authentication.enforce_authentication(conn, [])
+      assert conn.halted
+    end
+
+    test "enforce_authentication continues when the current account is set",
+         %{conn: conn} do
+      conn =
+        conn
+        |> assign(:current_account, %Palapa.Accounts.Account{})
+        |> Authentication.enforce_authentication([])
+
+      refute conn.halted
+    end
+
+    test "start_session puts the user in the session", %{conn: conn} do
+      {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
+
+      login_conn =
+        conn
+        |> Authentication.start_session(account)
+        |> send_resp(:ok, "")
+
+      assert get_session(login_conn, :account_id) == account.id
+    end
+
+    test "logout drops the session", %{conn: conn} do
+      logout_conn =
+        conn
+        |> put_session(:account_id, 123)
+        |> Authentication.logout()
+        |> send_resp(:ok, "")
+
+      next_conn = get(logout_conn, "/")
+      refute get_session(next_conn, :account_id)
+    end
+
+    test "call/2 places the current account into assigns", %{conn: conn} do
+      {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
+
+      conn =
+        conn
+        |> put_session(:account_id, account.id)
+        |> Authentication.call([])
+
+      assert conn.assigns.current_account.id == account.id
+    end
+
+    test "call/2 with no session set current_account assign to nil", %{conn: conn} do
+      conn = Authentication.call(conn, [])
+      assert conn.assigns.current_account == nil
+    end
+
+    test "login with valid email and password", %{conn: conn} do
+      {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
+
+      {:ok, conn} =
+        Authentication.login_with_email_and_password(
+          conn,
+          "richard.hendricks@piedpiper.com",
+          "password"
+        )
+
+      assert get_session(conn, :account_id) == account.id
+    end
+
+    test "login with an unknown user", %{conn: conn} do
+      assert {:error, :unauthorized, _conn} =
+               Authentication.login_with_email_and_password(
+                 conn,
+                 "unknown.account@none.com",
+                 "password"
+               )
+    end
+
+    test "login with password mismatch", %{conn: conn} do
+      {:ok, _} = Palapa.Accounts.Registrations.create(@registration)
+
+      assert {:error, :unauthorized, _conn} =
+               Authentication.login_with_email_and_password(
+                 conn,
+                 "richard.hendricks@piedpiper.com",
+                 "wrong_pass"
+               )
+    end
   end
 
-  test "enforce_authentication halts when no current_account exists", %{conn: conn} do
-    conn = Authentication.enforce_authentication(conn, [])
-    assert conn.halted
-  end
+  describe "workspace context retrieval" do
+    setup do
+      workspace = insert_pied_piper!()
+      conn = login(workspace.richard)
+      {:ok, conn: conn, workspace: workspace}
+    end
 
-  test "enforce_authentication continues when the current account is set",
-       %{conn: conn} do
-    conn =
-      conn
-      |> assign(:current_account, %Palapa.Accounts.Account{})
-      |> Authentication.enforce_authentication([])
+    test "gets redirected when a workspace is not active", %{conn: conn, workspace: workspace} do
+      Palapa.Organizations.delete(workspace.organization, workspace.richard)
 
-    refute conn.halted
-  end
+      conn = get(conn, Routes.dashboard_path(conn, :index, workspace.organization))
 
-  test "start_session puts the user in the session", %{conn: conn} do
-    {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
-
-    login_conn =
-      conn
-      |> Authentication.start_session(account)
-      |> send_resp(:ok, "")
-
-    assert get_session(login_conn, :account_id) == account.id
-  end
-
-  test "logout drops the session", %{conn: conn} do
-    logout_conn =
-      conn
-      |> put_session(:account_id, 123)
-      |> Authentication.logout()
-      |> send_resp(:ok, "")
-
-    next_conn = get(logout_conn, "/")
-    refute get_session(next_conn, :account_id)
-  end
-
-  test "call/2 places the current account into assigns", %{conn: conn} do
-    {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
-
-    conn =
-      conn
-      |> put_session(:account_id, account.id)
-      |> Authentication.call([])
-
-    assert conn.assigns.current_account.id == account.id
-  end
-
-  test "call/2 with no session set current_account assign to nil", %{conn: conn} do
-    conn = Authentication.call(conn, [])
-    assert conn.assigns.current_account == nil
-  end
-
-  test "login with valid email and password", %{conn: conn} do
-    {:ok, %{account: account}} = Palapa.Accounts.Registrations.create(@registration)
-
-    {:ok, conn} =
-      Authentication.login_with_email_and_password(
-        conn,
-        "richard.hendricks@piedpiper.com",
-        "password"
-      )
-
-    assert get_session(conn, :account_id) == account.id
-  end
-
-  test "login with an unknown user", %{conn: conn} do
-    assert {:error, :unauthorized, _conn} =
-             Authentication.login_with_email_and_password(
-               conn,
-               "unknown.account@none.com",
-               "password"
-             )
-  end
-
-  test "login with password mismatch", %{conn: conn} do
-    {:ok, _} = Palapa.Accounts.Registrations.create(@registration)
-
-    assert {:error, :unauthorized, _conn} =
-             Authentication.login_with_email_and_password(
-               conn,
-               "richard.hendricks@piedpiper.com",
-               "wrong_pass"
-             )
+      assert redirected_to(conn, 302) =~ Routes.organization_path(conn, :index)
+    end
   end
 end
--- test/palapa_web/controllers/organization_controller_test.exs
+++ test/palapa_web/controllers/organization_controller_test.exs
@@ -4,11 +4,17 @@ defmodule PalapaWeb.OrganizationControllerTest do
   setup do
     workspace = insert_pied_piper!()
     conn = login(workspace.richard)
-    {:ok, conn: conn}
+    {:ok, conn: conn, workspace: workspace}
   end
 
-  test "display the list of workspaces", %{conn: conn} do
+  test "display the list of active workspaces", %{conn: conn, workspace: workspace} do
     conn = get(conn, Routes.organization_path(conn, :index))
-    assert html_response(conn, 200) =~ "workspaces"
+    assert html_response(conn, 200) =~ "Pied Piper"
+  end
+
+  test "deleted workspaces are not visible", %{conn: conn, workspace: workspace} do
+    Palapa.Organizations.delete(workspace.organization, workspace.richard)
+    conn = get(conn, Routes.organization_path(conn, :index))
+    assert html_response(conn, 200) =~ "You are not a member of any workspace"
   end
 end
