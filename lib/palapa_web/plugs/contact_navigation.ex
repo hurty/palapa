d803bda6838dc6685e2e58981fceaf1d4feb94f7
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-10-28 08:34:28 +0100

    Add contacts breadcrumbs

--- lib/palapa_web/plugs/contact_navigation.ex
+++ lib/palapa_web/plugs/contact_navigation.ex
@@ -1,5 +1,7 @@
 defmodule PalapaWeb.ContactNavigation do
   import Plug.Conn
+  import PalapaWeb.Breadcrumbs
+  alias PalapaWeb.Router.Helpers, as: Routes
 
   def init(options) do
     options
@@ -8,5 +10,9 @@ defmodule PalapaWeb.ContactNavigation do
   def call(conn, _params) do
     conn
     |> assign(:navigation, "contacts")
+    |> put_breadcrumb(
+      "Contacts",
+      Routes.live_path(conn, PalapaWeb.ContactLive, conn.assigns.current_organization)
+    )
   end
 end
