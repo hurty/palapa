d3d2602d72cb13303b77af38d29ae4617adbbece
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-11-09 18:11:43 +0100

    get port from env variable

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :palapa, PalapaWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "palapabeta.cleverapps.io", port: 80],
+  url: [host: "palapabeta.cleverapps.io", port: String.to_integer(System.get_env("PORT"))],
   force_ssl: [hsts: true, rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
