5698a328ff181ccf0d8dadaca753315f0d2b35dd
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-11-19 14:43:47 +0100

    Display last messages on dashboard

--- lib/palapa_web/controllers/dashboard_controller.ex
+++ lib/palapa_web/controllers/dashboard_controller.ex
@@ -1,6 +1,8 @@
 defmodule PalapaWeb.DashboardController do
   use PalapaWeb, :controller
   alias Palapa.Dashboard
+  alias Palapa.Messages
+  alias Palapa.Teams
 
   plug(:put_navigation, "dashboard")
   plug(:put_common_breadcrumbs)
@@ -13,10 +15,38 @@ defmodule PalapaWeb.DashboardController do
     )
   end
 
-  def index(conn, _params) do
+  def index(conn, params) do
     with :ok <- permit(Dashboard.Policy, :index_dashboard, current_member(conn)) do
       events = Palapa.Events.last_50_events(current_organization(conn), current_member(conn))
-      render(conn, "index.html", events: events)
+
+      selected_team =
+        if params["team_id"] do
+          Teams.where_organization(current_organization(conn))
+          |> Teams.get!(params["team_id"])
+        end
+
+      messages =
+        Messages.visible_to(current_member(conn))
+        |> filter_team(selected_team)
+        |> Messages.paginate(params["page"])
+
+      teams = Teams.list_for_member(current_member(conn))
+
+      render(conn, "index.html",
+        events: events,
+        messages: messages,
+        teams: teams,
+        selected_team: selected_team
+      )
+    end
+  end
+
+  defp filter_team(query, team) do
+    if team do
+      query
+      |> Messages.published_to(team)
+    else
+      query
     end
   end
 end
